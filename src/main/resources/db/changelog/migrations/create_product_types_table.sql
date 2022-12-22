9c3ba86c207a7dddc5e788863df2194e50438a8b
Lennin Hdz <lennin92@gmail.com>
2022-12-22 10:09:18 -0600

    :card_file_box: Added database migrations v1

---
+++ src/main/resources/db/changelog/migrations/create_product_types_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE product_type (
+    id VARCHAR(26) NOT NULL PRIMARY KEY,
+    product_type_name VARCHAR(50) NOT NULL
+);
---
+++ src/main/resources/db/changelog/migrations/insert_product_types.sql
@@ -0,0 +1,8 @@
+INSERT INTO product_type (id, product_type_name)
+    values ('01GMX95VA7MJEC6GM6BBHWG9F0','Bienes de consumo'),
+           ('01GMX968WARGNCNTW1STTCB309','Bienes de uso comun'),
+           ('01GMX96D1VWMWJJMEGYJTNXCXZ','Bienes de emergencia'),
+           ('01GMX964EG9QEQG11JYNSNTSE9','Bienes durables'),
+           ('01GMX96Q0JVTQA829DRM41ZXMN','Bienes de especialidad'),
+           ('01GMX96Q0JBKHE8ENAW87H0MB8','Productos industriales'),
+           ('01GMX95YY0072TZ3XTYP0Y59F7','Mezcla de producto');
