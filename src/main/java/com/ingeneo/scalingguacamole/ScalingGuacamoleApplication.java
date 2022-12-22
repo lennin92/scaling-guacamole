817363456ec2141d65a2bc9064c5535af7c3f828
Lennin Hdz <lennin@fituapp.com>
2022-12-22 08:53:57 -0600

    :tada: projects setup

---
+++ src/main/java/com/ingeneo/scalingguacamole/ScalingGuacamoleApplication.java
@@ -0,0 +1,13 @@
+package com.ingeneo.scalingguacamole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalingGuacamoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalingGuacamoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ingeneo/scalingguacamole/ScalingGuacamoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingeneo.scalingguacamole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalingGuacamoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
